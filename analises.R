fd6aaf398bf66e872b0393d23e917450c1fb6635
saulosgil <saulosgil@hotmail.com>
2023-06-23 18:52:37 -0300

    frequencias relativas

--- analises.R
+++ analises.R
@@ -58,7 +58,7 @@ df <- bind_rows(low_ts,hig_ts)
 df <-
   df |>
   mutate(ansiedade_class = if_else(ansiedade_score > 25, 1,0),
-         depressao_class = if_else(depressao_score > 28, 1,0),
+         depressao_class = if_else(depressao_score >= 29, 1,0),
          idade_class = if_else(idade < 65, "adulto", "idoso"),
          bmi = peso/((estatura/100)^2),
          bmi_class = if_else(bmi < 30, "non-obese", "obese",missing = "non-obese")
@@ -75,7 +75,9 @@ df <-
   relocate(idade_class,.after = idade) |>
   relocate(bmi,.after = estatura) |>
   relocate(bmi_class,.after = bmi) |>
-  mutate(raca = as.factor(raca))
+  mutate(raca = as.factor(raca)) |>
+  mutate(ansiedade_class = as.factor(ansiedade_class)) |>
+  mutate(depressao_class = as.factor(depressao_class))
 
 glimpse(df)
 
@@ -126,6 +128,136 @@ df |>
     axis.title.x = element_blank()
   )
 
+# Grafico de barras para mostrar frequencia relativa de sintomas severos de ansiedade  --------
+# handgrip ------------------------------------------------------------------------------------
+# Ansiedade
+hgs_ansiedade_severa <-
+  df |>
+  group_by(hgs_class, ansiedade_class) |>
+  summarise(abs_ansiedade_severo = n(),
+            perc_ansiedade_severo = n()/(nrow(df)/2)*100)
+
+hgs_ansiedade_severa
+
+# plot
+hgs_ans <-
+hgs_ansiedade_severa |>
+  filter(ansiedade_class == 1) |>
+  ggplot(mapping = aes(x = hgs_class,
+                       y = perc_ansiedade_severo)) +
+  geom_col(mapping = aes(),
+           color = "black",
+           fill = c("black", "white")) +
+  geom_text(aes(label = c("42.6%", "67.6%")),
+            size = 8,
+            vjust = 0,
+            hjust = -0.2) +
+  ylab(label = "Relative frequency (%)") +
+  xlab(label = "") +
+  scale_x_discrete(labels = c("High\nStrength", "Low\nStrength"))+
+  theme(panel.grid = element_line(colour = "black",
+                                  linetype = 2))  +
+  theme_classic() +
+  scale_y_continuous(limits = c(0, 80)) +
+  coord_flip()
+
+# Depressao
+hgs_depressao_severa <-
+  df |>
+  group_by(hgs_class, depressao_class) |>
+  summarise(abs_depressao_severo = n(),
+            perc_depressao_severo = n()/(nrow(df)/2)*100)
+
+hgs_depressao_severa
+
+# Plot
+hgs_dep <-
+  hgs_depressao_severa |>
+  filter(depressao_class == 1) |>
+  ggplot(mapping = aes(x = hgs_class,
+                       y = perc_depressao_severo)) +
+  geom_col(mapping = aes(),
+           color = "black",
+           fill = c("black", "white")) +
+  geom_text(aes(label = c("2.7%", "7.4%")),
+            # position = position_dodge(0.9),
+            size = 8,
+            vjust = 0,
+            hjust = -0.2) +
+  ylab(label = "Relative frequency (%)") +
+  xlab(label = "") +
+  scale_x_discrete(labels = c("High\nStrength", "Low\nStrength"))+
+  theme(panel.grid = element_line(colour = "black",
+                                  linetype = 2))  +
+  theme_classic() +
+  scale_y_continuous(limits = c(0, 10)) +
+  coord_flip()
+
+# Timed stands --------------------------------------------------------------------------------
+# Ansiedade
+ts_ansiedade_severa <-
+  df |>
+  group_by(ts_class, ansiedade_class) |>
+  summarise(abs_ansiedade_severo = n(),
+            perc_ansiedade_severo = n()/(nrow(df)/2)*100)
+
+ts_ansiedade_severa
+
+# plot
+ts_ans <-
+  ts_ansiedade_severa |>
+  filter(ansiedade_class == 1) |>
+  ggplot(mapping = aes(x = ts_class,
+                       y = perc_ansiedade_severo)) +
+  geom_col(mapping = aes(),
+           color = "black",
+           fill = c("black", "white")) +
+  geom_text(aes(label = c("42.6%", "67.6%")),
+            size = 8,
+            vjust = 0,
+            hjust = -0.2) +
+  ylab(label = "Relative frequency (%)") +
+  xlab(label = "") +
+  scale_x_discrete(labels = c("High\nStrength", "Low\nStrength"))+
+  theme(panel.grid = element_line(colour = "black",
+                                  linetype = 2))  +
+  theme_classic() +
+  scale_y_continuous(limits = c(0, 80)) +
+  coord_flip()
+
+# Depressao
+ts_depressao_severa <-
+  df |>
+  group_by(ts_class, depressao_class) |>
+  summarise(abs_depressao_severo = n(),
+            perc_depressao_severo = n()/(nrow(df)/2)*100)
+
+ts_depressao_severa
+
+# Plot
+ts_dep <-
+  ts_depressao_severa |>
+  filter(depressao_class == 1) |>
+  ggplot(mapping = aes(x = ts_class,
+                       y = perc_depressao_severo)) +
+  geom_col(mapping = aes(),
+           color = "black",
+           fill = c("black", "white")) +
+  geom_text(aes(label = c("1.8%", "8.3%")),
+            # position = position_dodge(0.9),
+            size = 8,
+            vjust = 0,
+            hjust = -0.2) +
+  ylab(label = "Relative frequency (%)") +
+  xlab(label = "") +
+  scale_x_discrete(labels = c("High\nStrength", "Low\nStrength"))+
+  theme(panel.grid = element_line(colour = "black",
+                                  linetype = 2))  +
+  theme_classic() +
+  scale_y_continuous(limits = c(0, 10)) +
+  coord_flip()
+
+(hgs_ans | hgs_dep)/(ts_ans | ts_dep)
 # analises exploratórias - test t e regressão -------------------------------------------------
 # tirar notação cientifica
 options(scipen=999)
@@ -158,9 +290,13 @@ t.test(depressao_score ~ ts_class, df)
 model <- lm(whoqol_fisico ~ hgs_max +
                genero + idade_class + raca + bmi_class, df)
 
-model <- glm(depressao_class ~ hgs_class +
-              genero + idade_class + raca + bmi_class, df, family = binomial)
+model <- glm(ansiedade_class ~ hgs_class +
+              genero + idade_class + raca + renda + bmi_class, df, family = binomial)
 
 summary(model)
+
+
+
+
 
 
